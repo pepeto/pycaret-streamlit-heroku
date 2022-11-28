3dc44f1a565723c69c444c99c6d2b75ddc1ec6a1
AVB <pepeto@users.noreply.github.com>
2022-11-28 19:54:47 -0300

    b

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 from pycaret.regression import load_model, predict_model
 import streamlit as st
 import pandas as pd
-import numpy as np
+# import numpy as np
 
 model = load_model('deployment_28042020')
+
 
 def predict(model, input_df):
     predictions_df = predict_model(estimator=model, data=input_df)
     predictions = predictions_df['Label'][0]
     return predictions
 
-def run():
 
+def run():
     from PIL import Image
     image = Image.open('logo.png')
     image_hospital = Image.open('hospital.jpg')
 
-    st.image(image,use_column_width=False)
+    st.image(image, use_column_width=False)
 
     add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
-    ("Online", "Batch"))
+        "How would you like to predict?",
+        ("Online", "Batch"))
 
     st.sidebar.info('This app is created to predict patient hospital charges')
     st.sidebar.success('https://www.pycaret.org')
-    
+
     st.sidebar.image(image_hospital)
 
     st.title("Insurance Charges Prediction App")
@@ -34,16 +35,16 @@ def run():
         age = st.number_input('Age', min_value=1, max_value=100, value=25)
         sex = st.selectbox('Sex', ['male', 'female'])
         bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-        children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
+        children = st.selectbox('Children', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         if st.checkbox('Smoker'):
             smoker = 'yes'
         else:
             smoker = 'no'
         region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
 
-        output=""
+        output = ""
 
-        input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
+        input_dict = {'age': age, 'sex': sex, 'bmi': bmi, 'children': children, 'smoker': smoker, 'region': region}
         input_df = pd.DataFrame([input_dict])
 
         if st.button("Predict"):
@@ -58,8 +59,9 @@ def run():
 
         if file_upload is not None:
             data = pd.read_csv(file_upload)
-            predictions = predict_model(estimator=model,data=data)
+            predictions = predict_model(estimator=model, data=data)
             st.write(predictions)
+
 
 if __name__ == '__main__':
     run()
